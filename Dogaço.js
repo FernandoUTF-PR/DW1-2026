7029ceee42aab27dcfe68a68969e95501aa713f8
FernandoUTF-PR <fernandomoresquimedeiros@gmail.com>
2026-04-14 11:29:30 -0300

    Add files via upload

---
+++ Dogaço.js
@@ -0,0 +1,29 @@
+function funcaoDelicia() {
+
+    let basico = document.getElementById("HotDogBase").value;
+    let duplo = document.getElementById("HotDogDouble").value;
+    let salada = parseFloat(document.getElementById("Xsalada").value);
+    let R350mL = document.getElementById("Refri350ml").value;
+    let R1L = parseFloat(document.getElementById("Refri1L").value);
+
+    let basicoT = (basico * 22.00);
+    let duploT = (duplo * 26.00);
+    let saladaT = (salada * 29.00);
+    let R350mlT = (R350mL * 5.00);
+    let R1LT = (R1L * 8.00)
+
+
+let total = basicoT + duploT + saladaT + R350mlT + R1LT;
+
+    if (total === 0) {
+        footer.classList.add("erro");
+        mensagem.innerHTML = "PQ VC QUIS PEDIR EM PRIMEIRO LUGAR";
+        document.getElementById("respTotal").innerHTML = "-";
+    } else if (total > 0) {
+        alert(total);
+         mensagem.innerHTML = "Preço total";
+        }
+
+        footer.classList.remove("erro");
+        document.getElementById("respTotal").innerHTML = total.toFixed(2);
+    }
