d007b30718eb7398b00062be002ffe3b7357754e
FERNANDO MORESQUI MEDEIROS <fernandomoresquimedeiros@gmail.com>
2026-09-08 11:28:45 -0300

    ana com tres n

--- CRUDproduto/crudproduto.sql
+++ CRUDproduto/crudproduto.sql
@@ -1,16 +1,16 @@
 DROP TABLE IF EXISTS public.produto;
 
 CREATE TABLE public.produto (
-    id INT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
+    id_produto INT PRIMARY KEY,
+    nome_produto VARCHAR(50) NOT NULL,
     tamanho CHAR(1),
     peso FLOAT
 );
 
-INSERT INTO public.produto (id, nome, tamanho, peso) VALUES
+INSERT INTO public.produto (id_produto, nome_produto, tamanho, peso) VALUES
 (1, 'Borracha', 'M', '0.2');
-INSERT INTO public.aluno (id, nome, tamanho, peso) VALUES
+INSERT INTO public.produto (id_produto, nome_produto, tamanho, peso) VALUES
 (2, 'Bola', 'G', '2.3');
 
 
-select * from public.produto
+select * from public.produto;
